e6f02eab3ed27e25c726411a851acb88101c177e
PhanTruongT <bplhp18@gmail.com>
2020-04-19 22:18:19 -0400

    fleshed out basic login, comment streaming, and keyword search

--- bot.py
+++ bot.py
@@ -1,12 +1,43 @@
-import time
+
 import praw
+import logging
 import config
 
-#(config.client_id, config.client_secret,
-#                    config.password, config.user_agent,
-#                   config.username)
+import requests
+import json
+
+#TODO get logger
 
 
-user_agent = "Karma breakdown 1.0 by /u/_Daimon_"
-r = praw.Reddit(user_agent=user_agent)
-r.login(config.username,config.password)
+def main():
+
+    #Attempt login
+    try:
+        reddit = praw.Reddit(client_id=config.client_id,
+                        client_secret=config.client_secret,
+                        password=config.password,
+                        user_agent=config.user_agent,
+                        username=config.username)
+    
+    except:
+        #TODO error handling
+        pass
+
+    #iterate through newly submitted comments
+    for comment_id in reddit.subreddit(config.subreddits).stream.comments(skip_existing=True):
+        
+        #Search for keyword (with valid spacing)
+        if reddit.comment(comment_id).body.find(config.keyword + ' ',0,6):
+            #TODO reply to comment with link and add to postgres db
+            print('command received')
+            reddit.comment(comment_id).reply('PLACEHOLDER')
+            
+        
+
+
+    
+
+if __name__ == "__main__":
+    main()
+
+    
